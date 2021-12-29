3b6ac90110b206df311942c6e8fdbd549e92d8dc
Aleksandar Cvjetean <31933549+crypt096@users.noreply.github.com>
2021-12-29 21:38:19 +0100

    Update tailwind.config.js

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -1,14 +1,11 @@
 module.exports = {
-  plugins: [
-    require('daisyui'),
+  content: [
+    "./src/**/*.{js,jsx,ts,tsx}",
   ],
-  daisyui: {
-    styled: true,
-    themes: true,
-    base: true,
-    utils: true,
-    logs: true,
-    rtl: false,
+  theme: {
+    extend: {},
   },
-
+  plugins: [
+    require('daisyui')
+  ],
 }
