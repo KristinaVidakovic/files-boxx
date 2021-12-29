df68b5d3173b88fff522de51010c08962ad60f3f
Aleksandar Cvjetean <31933549+crypt096@users.noreply.github.com>
2021-12-29 21:49:04 +0100

    Add Hero component

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,9 +1,10 @@
-import Navbar from "./components/Navbar";
+import Hero from "./components/Hero";
 
 function App() {
+  console.log(window)
   return (
     <div className="App">
-      <Navbar />
+      <Hero />
     </div>
   );
 }
---
+++ frontend/src/components/Hero/index.tsx
@@ -0,0 +1,21 @@
+const Hero = () => {
+  return (
+    <div className="hero min-h-screen bg-base-200">
+      <div className="text-center hero-content">
+        <div className="max-w-md">
+          <h1 className="mb-5 text-5xl font-bold">
+            Welcome to
+          </h1>
+          <p className="mb-5">
+            FilesBoxx
+          </p>
+          <button className="btn btn-primary">
+            Try me today!
+          </button>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default Hero
